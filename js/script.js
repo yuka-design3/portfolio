3f6e75c9f9c76811cc019f2b093d880f10a56ff4
yuka-design3 <120924679+yuka-design3@users.noreply.github.com>
2023-03-24 04:46:04 +0900

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
+// ローディング画面
+$(function(){
+
+  setTimeout(() => {
+    $('.logo').addClass('red');
+  }, "500")
+
+  setTimeout(() => {
+    $('.logo').addClass('orange');
+  }, "1000")
+
+  setTimeout(() => {
+    $('.logo').addClass('yellow');
+  }, "1500")
+
+  setTimeout(() => {
+    $('.logo').addClass('blue');
+  }, "2000")
+
+  setTimeout(() => {
+    $('.logo').addClass('green');
+  }, "2500")
+
+  setTimeout(() => {
+    $('.loading').fadeOut();
+  }, "2000")
+})
+
+
 // 秀英丸ゴシックの指定
 
-(function (d) {
-    var config = {
-        kitId: 'gmh2slm',
-        scriptTimeout: 3000,
-        async: true
-    },
-        h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
+(function(d) {
+  var config = {
+    kitId: 'gmh2slm',
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
 
 // Alana Smooth Regularの指定
@@ -31,3 +60,4 @@ $(function () {
 
     });
 });
+
