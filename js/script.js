0548ca09b323f9ed6cd16a852b0bf3ec2fb027e3
yuka-design3 <120924679+yuka-design3@users.noreply.github.com>
2023-03-25 03:39:46 +0900

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
-// ローディング画面
-$(function(){
-
-  setTimeout(() => {
-    $('.logo').addClass('red');
-  }, "500")
-
-  setTimeout(() => {
-    $('.logo').addClass('orange');
-  }, "1000")
-
-  setTimeout(() => {
-    $('.logo').addClass('yellow');
-  }, "1500")
-
-  setTimeout(() => {
-    $('.logo').addClass('blue');
-  }, "2000")
-
-  setTimeout(() => {
-    $('.logo').addClass('green');
-  }, "2500")
-
-  setTimeout(() => {
-    $('.loading').fadeOut();
-  }, "2000")
-})
-
-
 // 秀英丸ゴシックの指定
 
 (function(d) {
@@ -50,6 +21,34 @@ $(function(){
   })(document);
 
 
+// ローディング画面
+$(function(){
+
+  setTimeout(() => {
+    $('.logo_line').addClass('logo_red');
+  }, "500")
+
+  setTimeout(() => {
+    $('.logo_line').addClass('logo_orange');
+  }, "1000")
+
+  setTimeout(() => {
+    $('.logo_line').addClass('logo_yellow');
+  }, "1500")
+
+  setTimeout(() => {
+    $('.logo_line').addClass('logo_blue');
+  }, "2000")
+
+  setTimeout(() => {
+    $('.logo_line').addClass('logo_green');
+  }, "2500")
+
+  setTimeout(() => {
+    $('.loading').fadeOut();
+  }, "3000")
+})
+
 
 // ハンバーガーメニュー
 
@@ -61,3 +60,57 @@ $(function () {
     });
 });
 
+// 下からフェードイン
+$(function () {
+  $(window).scroll(function () {
+    const windowHeight = $(window).height();
+    const scroll = $(window).scrollTop();
+
+    $('.works_item').each(function () {
+      const targetPosition = $(this).offset().top;
+      if (scroll > targetPosition - windowHeight + 100) {
+        $(this).addClass("is-fadein");
+      }
+    });
+  });
+});
+
+
+// 下からフェードイン
+$(function () {
+  $(window).scroll(function () {
+    const windowHeight = $(window).height();
+    const scroll = $(window).scrollTop();
+
+    $('.top_mockup, .img02, .img03').each(function () {
+      const targetPosition = $(this).offset().top;
+      if (scroll > targetPosition - windowHeight + 100) {
+        $(this).addClass("is-fadein");
+      }
+    });
+  });
+});
+
+
+// pagetop 途中から表示
+
+$(function() {
+  // 変数にクラスを入れる
+  var btn = $('.pagetop');
+  
+  //スクロールしてページトップから100に達したらボタンを表示
+  $(window).on('load scroll', function(){
+    if($(this).scrollTop() > 300) {
+      btn.addClass('active');
+    }else{
+      btn.removeClass('active');
+    }
+  });
+
+  //スクロールしてトップへ戻る
+  btn.on('click',function () {
+    $('body,html').animate({
+      scrollTop: 0
+    });
+  });
+});
